a2738eee94f6142b82009e19fe80003b3c89a38c
Michael Chung <mikechung89@gmail.com>
2022-04-18 17:43:27 -0400

    routing for climates started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import NavBar from './NavBar'
 import Home from './Home'
 import './styles/App.css';
-import './images/hero.png'
-import Home from './Home';
 import SpacingGrid from './Grid'
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   
@@ -12,10 +10,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/listings" element={<SpacingGrid />} />
-      {/*<Route path="mylistings" element={<Invoices />} /> */}
-    </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listings" element={<SpacingGrid />}>
+          {/* <Route path="/listings/:climate" element={<ClimateListing />} />  */}
+        </Route>
+      </Routes>
     </div>
   );
 }
