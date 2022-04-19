2bb33f7a9284c226ceeea918f0c573020f33aece
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-19 13:16:07 -0400

    changes

--- src/ListingPage.js
+++ src/ListingPage.js
@@ -11,6 +11,7 @@ function ListingPage() {
   };
 
   const [listings, setListings] = useState([])
+  
 
   useEffect(() => {
     fetch("http://localhost:9292/listings")
@@ -18,24 +19,43 @@ function ListingPage() {
       .then(data => setListings(data))
   }, [])
 
+  const reviewList = listings.map((listing) => listing.reviews)
+  
+
   const mappedListings = listings.map(listing => {
     return (
       <ListingCard 
         key={listing.id} 
         location={listing.location} 
         description={listing.description} 
-        price={listing.price_per_day} 
+        price={listing.price_per_day}
+        type={listing.climate_type} 
         title={listing.title} 
         date={listing.created_at} 
-        image={listing.thumbnail} />)
-      })
+        image={listing.thumbnail}
+        comment={listing.reviews.map((review) => {
+          return(
+            <>
+            <li>{review.user.name} said: {review.comment}<br>
+            </br>
+            Rating: {review.rating}/10
+            </li>
+            </>
+          )
+        })}
+        
+         />
+  )})
 
+      
+      console.log(reviewList)
   return (
     <div className='grid'>
       <Grid sx={{ flexGrow: 1 }} container spacing={2}>
         <Grid item xs={12}>
           <Grid container justifyContent="center" spacing={spacing}>
             {mappedListings}
+            
           </Grid>
         </Grid>
       </Grid>
--- src/routes/FilteredListings.js
+++ src/routes/FilteredListings.js
@@ -1,18 +1,57 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react'
+import Grid from '@mui/material/Grid';
+import ListingCard from "../ListingCard";
+import { Outlet } from "react-router-dom"
 
 function FilteredListings () {
     let params = useParams()
     const [climateListing, setClimateListing] = useState([])
+    const [spacing, setSpacing] = useState(2);
+
+  const handleChange = (event) => {
+    setSpacing(Number(event.target.value));
+  };
 
     useEffect(() => {
-        fetch(`http://localhost:9292/listings/${params.climate}`)
+        fetch(`http://localhost:9292/listings/climate/${params.climate}`)
           .then(res => res.json())
           .then(setClimateListing)
       }, [])
     console.log(climateListing)
 
-    return <h2>Hello World</h2>
+    const mappedClimateListing = climateListing.map(listing => {
+      return (
+        <ListingCard 
+          key={listing.id} 
+          location={listing.location} 
+          description={listing.description} 
+          price={listing.price_per_day}
+          type={listing.climate_type}
+          title={listing.title} 
+          date={listing.created_at} 
+          image={listing.thumbnail}
+          comment={listing.reviews.map((review) => {
+            return(
+              <li>{review.user.name} said: {review.comment}  Rating: {review.rating}/10</li>
+            )
+          })}
+          
+           />
+    )})
+
+    return (
+      <div className='grid'>
+      <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={spacing}>
+            {mappedClimateListing}
+          </Grid>
+        </Grid>
+      </Grid>
+      <Outlet />
+    </div>
+    )
 
 }
 
